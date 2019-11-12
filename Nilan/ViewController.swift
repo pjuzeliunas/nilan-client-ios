96a2c8a9634c2b8ed6422cd8b772dee28a11fa61
Povilas Juzeliunas <pjuzeliunas@gmail.com>
2019-11-12 21:25:02 +0100

    Initial setup

---
+++ Nilan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nilan
+//
+//  Created by Povilas Juzeliunas on 12/11/2019.
+//  Copyright © 2019 Povilas Juzeliunas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
